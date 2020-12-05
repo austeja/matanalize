026d3713226a31865a15e20a7c8031ef022dda41
Darius Pažusinskis <p998xtj@fspa.myntet.se>
2020-12-05 16:56:40 +0200

    Lagrange

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace Matanalize
         {
         static void Main(string[] args)
             {
-            Console.WriteLine("Hello World!");
+                Lagrange lagrange = new Lagrange();
+                
+                // 2. Užrašykite liekamąjį narį 
+                lagrange.iterate(-1, 1);
+                
+                // 3. Įvertinkite liekamąjį narį. 
+                lagrange.iterate(-0.5, 0.5);
+                lagrange.iterate(-0.1, 0.1);
+                lagrange.iterate(-0.001, 0.001);
             }
         }
     }
+
+class Lagrange
+{
+    public void iterate(Double fromInterval, Double toInterval)
+    {
+        Double o = 1;
+        for(Double i = fromInterval; i <= toInterval; i+=toInterval)
+        {
+            Console.WriteLine("n={0}, {1}", o , calculateRemainder(i));
+            o++;
+        }
+        Console.WriteLine();
+    }
+    
+    public Double calculateRemainder(Double x)
+    {
+        Double e = Math.E;
+        Double y = Math.Pow(e, x) * Math.Sin(x);
+
+        return y;
+    }
+    
+    
+}
