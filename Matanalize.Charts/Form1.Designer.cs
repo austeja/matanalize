24a836f4de6ace61ebe90946d0720bb442510d2f
Austeja Kalpakovaite <austeja.kalpakovaite@bentley.com>
2020-12-17 22:06:49 +0200

    Fixed graphs

--- Matanalize.Charts/Form1.Designer.cs
+++ Matanalize.Charts/Form1.Designer.cs
@@ -20,8 +20,8 @@ namespace Matanalize.Charts
         private PlotView plot5;
         private PlotView plot6;
 
-        private int _intervalStart = -50;
-        private int _intervalEnd = 50;
+        private double _intervalStart = -2;
+        private double _intervalEnd = 2;
 
         private IContainer components = null;
 
@@ -53,37 +53,62 @@ namespace Matanalize.Charts
 
             this.plot1 = new PlotView();
             InitializePlotProps(plot1, "b) kai imamas narys tik su x0", 1);
-            Func<double, double> taylorWithX0 = (double x) => Taylor.GetNthTaylorFormulaCoefficient(0); // will result in a constant 0
+            Func<double, double> taylorWithX0 =
+                (double x) => Taylor.GetNthTaylorFormulaCoefficient(0); // will result in a constant 0
             InitializePlotModel(plot1, taylorWithX0);
             this.Controls.Add(this.plot1);
 
             this.plot2 = new PlotView();
             InitializePlotProps(plot2, "c) kai imami nariai su x0 ir x1", 2);
-            Func<double, double> taylorWithX1 = (double x) => taylorWithX0(x) + Taylor.GetNthTaylorFormulaCoefficient(1) * x;
+            Func<double, double> taylorWithX1 =
+                (double x) =>
+                    Taylor.GetNthTaylorFormulaCoefficient(0) +
+                    Taylor.GetNthTaylorFormulaCoefficient(1) * x;
             InitializePlotModel(plot2, taylorWithX1);
             this.Controls.Add(this.plot2);
 
             this.plot3 = new PlotView();
             InitializePlotProps(plot3, "d) kai imami nariai su x0, x1 ir x2", 3);
-            Func<double, double> taylorWithX2 = (double x) => taylorWithX0(x) + taylorWithX1(x) + Taylor.GetNthTaylorFormulaCoefficient(2) * Math.Pow(x, 2);
+            Func<double, double> taylorWithX2 =
+                (double x) =>
+                    Taylor.GetNthTaylorFormulaCoefficient(0) +
+                    Taylor.GetNthTaylorFormulaCoefficient(1) * x +
+                    Taylor.GetNthTaylorFormulaCoefficient(2) * Math.Pow(x, 2);
             InitializePlotModel(plot3, taylorWithX2);
             this.Controls.Add(this.plot3);
 
             this.plot4 = new PlotView();
             InitializePlotProps(plot4, "d) kai imami nariai su x0, x1, x2 ir x3", 4);
-            Func<double, double> taylorWithX3 = (double x) => taylorWithX0(x) + taylorWithX1(x) + taylorWithX2(x) + Taylor.GetNthTaylorFormulaCoefficient(3) * Math.Pow(x, 3);
+            Func<double, double> taylorWithX3 = (double x) =>
+                    Taylor.GetNthTaylorFormulaCoefficient(0) +
+                    Taylor.GetNthTaylorFormulaCoefficient(1) * x +
+                    Taylor.GetNthTaylorFormulaCoefficient(2) * Math.Pow(x, 2) +
+                    Taylor.GetNthTaylorFormulaCoefficient(3) * Math.Pow(x, 3);
             InitializePlotModel(plot4, taylorWithX3);
             this.Controls.Add(this.plot4);
 
             this.plot5 = new PlotView();
             InitializePlotProps(plot5, "d) kai imami nariai su x0, x1, x2, x3 ir x4", 5);
-            Func<double, double> taylorWithX4 = (double x) => taylorWithX0(x) + taylorWithX1(x) + taylorWithX2(x) + taylorWithX3(x) + Taylor.GetNthTaylorFormulaCoefficient(4) * Math.Pow(x, 4);
+            Func<double, double> taylorWithX4 =
+                (double x) =>
+                    Taylor.GetNthTaylorFormulaCoefficient(0) +
+                    Taylor.GetNthTaylorFormulaCoefficient(1) * x +
+                    Taylor.GetNthTaylorFormulaCoefficient(2) * Math.Pow(x, 2) +
+                    Taylor.GetNthTaylorFormulaCoefficient(3) * Math.Pow(x, 3) +
+                    Taylor.GetNthTaylorFormulaCoefficient(4) * Math.Pow(x, 4);
             InitializePlotModel(plot5, taylorWithX4);
             this.Controls.Add(this.plot5);
 
+
             this.plot6 = new PlotView();
             InitializePlotProps(plot6, "d) kai imami nariai su x0, x1, x2, x3, x4 ir x5", 6);
-            Func<double, double> taylorWithX5 = (double x) => taylorWithX0(x) + taylorWithX1(x) + taylorWithX2(x) + taylorWithX3(x) + taylorWithX4(x) + Taylor.GetNthTaylorFormulaCoefficient(5) * Math.Pow(x, 5);
+            Func<double, double> taylorWithX5 = (double x) =>
+                    Taylor.GetNthTaylorFormulaCoefficient(0) +
+                    Taylor.GetNthTaylorFormulaCoefficient(1) * x +
+                    Taylor.GetNthTaylorFormulaCoefficient(2) * Math.Pow(x, 2) +
+                    Taylor.GetNthTaylorFormulaCoefficient(3) * Math.Pow(x, 3) +
+                    Taylor.GetNthTaylorFormulaCoefficient(4) * Math.Pow(x, 4) +
+                    Taylor.GetNthTaylorFormulaCoefficient(5) * Math.Pow(x, 5);
             InitializePlotModel(plot6, taylorWithX5);
             this.Controls.Add(this.plot6);
             }
@@ -106,7 +131,9 @@ namespace Matanalize.Charts
         private void InitializePlotModel(PlotView plot, Func<double, double> func)
             {
             var model = new PlotModel { Title = plot.Name };
-            model.Series.Add(new FunctionSeries(func, _intervalStart, _intervalEnd, 0.1));
+            model.Series.Add(new FunctionSeries(func, _intervalStart, _intervalEnd, 0.01));
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Left, ExtraGridlines = new double[] { 0 }, ExtraGridlineThickness = 1, ExtraGridlineColor = OxyColors.Black, });
+            model.Axes.Add(new OxyPlot.Axes.LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, ExtraGridlines = new double[] { 0 }, ExtraGridlineThickness = 1, ExtraGridlineColor = OxyColors.Black, });
             plot.Model = model;
             }
 
